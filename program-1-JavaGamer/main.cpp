39e236d9c016b6dc6faba606aa20ee75cc68ceae
Alpha Sop <joeyluuson@gmail.com>
2022-09-08 17:09:37 -0500

    mergely

--- program-1-JavaGamer/main.cpp
+++ program-1-JavaGamer/main.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    while (true)
+    {
+        int option = -1;
+
+        // Temporary variable used to validate the input before putting it in option
+        char tempInput;
+
+        std::cout << "Please select an option: \n"
+                     "1) Encode a sentence\n"
+                     "2) Decode binary\n"
+                     "0) Quit\n"
+                     "Select Choice (1, 2, or 0): ";
+        cin >> tempInput;
+
+        // Validate the input and make sure it is a number
+        if (not (tempInput >= 48 and tempInput <= 57))
+        {
+            cout << "\nERROR: You selected an invalid choice, please try again\n" << endl;
+            continue;
+        }
+
+        option = tempInput - 48;
+
+        switch (option) {
+            case 0:
+                goto endLoop;
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                cout << "\nERROR: You selected an invalid choice, please try again\n" << endl;
+                break;
+        }
+    }
+
+    // this feels dirty but this is what goto is for...
+    // could use a flag but ehh?
+    endLoop:
+        cout << "\nThank you!! o(*￣▽￣*)ブ" << endl;
+        return 0;
 }
